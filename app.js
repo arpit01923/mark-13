88d7eba1ee7e911260a43772642ea9fb2fa329ab
Arpit <arpitkumar01923@gmail.com>
2021-09-24 22:01:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var input=document.querySelector("#i1");
+var btn=document.querySelector("#i2");
+var output=document.querySelector("#i3");
+
+function palindrome(){
+    console.log(input.value)
+}
+
+btn.addEventListener("click",palindrome)
